fe5dd4e18ce0c9a565169af7eb17b65c85264dbd
mariolopezDev <marioalbertolopez@uned.cr>
2024-08-01 18:36:11 -0600

    mux and handlers implementation

--- internal/taskstore/task.go
+++ internal/taskstore/task.go
@@ -1,10 +1,26 @@
 package taskstore
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Task struct {
 	ID   int       `json:"id"`
 	Text string    `json:"text"`
 	Tags []string  `json:"tags"`
 	Due  time.Time `json:"due"`
+}
+
+func (t *Task) Validate() error {
+	if t.Text == "" {
+		return fmt.Errorf("el campo Texto no puede estar vacío")
+	}
+	if t.Due.Before(time.Now()) {
+		return fmt.Errorf("la fecha de vencimiento no puede ser en el pasado")
+	}
+	if t.Due.IsZero() {
+		return fmt.Errorf("la fecha de vencimiento no puede estar vacía")
+	}
+	return nil
 }
--- internal/taskstore/taskstore.go
+++ internal/taskstore/taskstore.go
@@ -1,6 +1,8 @@
 package taskstore
 
 import (
+	"errors"
+	"fmt"
 	"sync"
 	"time"
 )
@@ -11,6 +13,12 @@ type TaskStore struct {
 	nextID int
 }
 
+var (
+	ErrTaskNotFound  = errors.New("tarea no encontrada")
+	ErrInvalidTaskID = errors.New("ID de tarea inválido")
+	ErrIDExists      = errors.New("ID de tarea ya existe")
+)
+
 func New() *TaskStore {
 	return &TaskStore{
 		tasks:  make(map[int]Task),
@@ -18,19 +26,75 @@ func New() *TaskStore {
 	}
 }
 
-func (ts *TaskStore) CreateTask(text string, tags []string, due time.Time) int {
-	ts.Lock()
-	defer ts.Unlock()
+// TaskStore CRUD operations
 
-	task := Task{
-		ID:   ts.nextID,
+func (ts *TaskStore) CreateTask(text string, tags []string, due time.Time) (int, error) {
+	task := &Task{
 		Text: text,
 		Tags: tags,
 		Due:  due,
 	}
+	if err := task.Validate(); err != nil {
+		return 0, err
+	}
 
-	ts.tasks[task.ID] = task
+	ts.Lock()
+	defer ts.Unlock()
+
+	task.ID = ts.nextID
+	ts.tasks[task.ID] = *task
 	ts.nextID++
 
-	return task.ID
+	return task.ID, nil
+}
+
+func (ts *TaskStore) GetTask(id int) (Task, error) {
+	ts.Lock()
+	defer ts.Unlock()
+
+	task, ok := ts.tasks[id]
+	if !ok {
+		return Task{}, fmt.Errorf("%w: ID %d", ErrTaskNotFound, id)
+	}
+	return task, nil
+}
+
+func (ts *TaskStore) GetAllTasks() []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+	tasks := make([]Task, 0, len(ts.tasks))
+	for _, task := range ts.tasks {
+		tasks = append(tasks, task)
+	}
+	return tasks
+}
+
+func (ts *TaskStore) UpdateTask(id int, text string, tags []string, due time.Time) error {
+	ts.Lock()
+	defer ts.Unlock()
+
+	task, ok := ts.tasks[id]
+	if !ok {
+		return fmt.Errorf("%w: ID %d", ErrTaskNotFound, id)
+	}
+	task.Text = text
+	task.Tags = tags
+	task.Due = due
+	if err := task.Validate(); err != nil {
+		return err
+	}
+	ts.tasks[id] = task
+	return nil
+}
+
+func (ts *TaskStore) DeleteTask(id int) error {
+	ts.Lock()
+	defer ts.Unlock()
+
+	if _, ok := ts.tasks[id]; !ok {
+		return fmt.Errorf("%w: ID %d", ErrTaskNotFound, id)
+	}
+	delete(ts.tasks, id)
+	return nil
 }
